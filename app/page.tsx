27018a178ba22cec23dd6d4e642c2a940297be98
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-03-20 20:52:52 -0500

    feat(Root): Add layout for Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,66 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import {
+  SunIcon,
+  BoltIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+    <div className="flex flex-col items-center justify-center text-gray-100 min-h-screen px-4">
+      <h1 className="text-4xl font-semibold mt-16 mb-10 text-center md:mt-0 md:mb-20">
+        ChatGPT
+      </h1>
+      <div className="flex flex-col gap-5 space-x-2 text-center md:flex-row md:gap-0">
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <SunIcon className="h-6 w-6" />
+            <h2 className="text-xl">Examples</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">
+              &quot;Explain quantum computing in simple terms&quot;
+            </p>
+            <p className="infoText">
+              &quot;What is the difference between a dog and a cat?&quot;
+            </p>
+            <p className="infoText">
+              &quot;Got any creative ideas for a 10 year old’s birthday?&quot;
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <BoltIcon className="h-6 w-6" />
+            <h2 className="text-xl">Capabilities</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">Change the ChatGPT Model to use</p>
+            <p className="infoText">
+              Messages are store in Firebase&apos;s Firestore
+            </p>
+            <p className="infoText">
+              Hot Toast notifications when ChatGPT is thinking!
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <ExclamationTriangleIcon className="h-6 w-6" />
+            <h2 className="text-xl">Limitations</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">
+              May occasionally generate incorrect information
+            </p>
+            <p className="infoText">
+              May occasionally produce harmful instructions
+            </p>
+            <p className="infoText">
+              Limited knowledge of world and events after 2021
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
