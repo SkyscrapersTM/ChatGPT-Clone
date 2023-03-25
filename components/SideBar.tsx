dd81e64770511247dfd305c1be5f7dc274f63654
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-03-25 17:10:38 -0500

    feat(Root): Added new components in Sidebar component

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -4,11 +4,25 @@ import NewChat from "@/components/NewChat";
 import { useEffect, useState } from "react";
 import { clsx } from "clsx";
 import { useSession, signOut } from "next-auth/react";
+import { db } from "@/firebase";
+import { collection, orderBy, query } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import ChatRow from "./ChatRow";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import ModelSelection from "./ModelSelection";
 
 const SideBar = () => {
   const [open, setOpen] = useState(false);
   const [windowWidth, setWindowWidth] = useState(0);
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createAt", "asc")
+      )
+  );
 
   useEffect(() => {
     setWindowWidth(window.innerWidth);
@@ -25,9 +39,9 @@ const SideBar = () => {
   };
 
   const sidebarClassNames = clsx(
-    "flex flex-col bg-[#202123] min-h-screen w-full max-w-xs md:min-w-[16rem] p-2",
+    "flex flex-col bg-[#202123] min-h-screen w-full max-w-xs md:min-w-[16rem] p-2 z-50",
     {
-      "absolute bg-opacity-50": windowWidth < 992,
+      "absolute bg-opacity-90": windowWidth < 992,
       hidden: windowWidth < 992 && !open,
     }
   );
@@ -38,8 +52,20 @@ const SideBar = () => {
           <div>
             <NewChat />
           </div>
-          <div>{/* ModelSelection */}</div>
-          {/* Map through the ChatRows */}
+          <div className="hidden sm:inline mt-2">
+            <ModelSelection />
+          </div>
+          <div className="flex flex-col mt-2 space-y-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading Chats</p>
+              </div>
+            )}
+            {/* Map through the ChatRows */}
+            {chats?.docs.map((chat) => (
+              <ChatRow key={chat.id} id={chat.id} />
+            ))}
+          </div>
         </div>
         {session && (
           <img
@@ -51,8 +77,8 @@ const SideBar = () => {
         )}
       </div>
       {windowWidth < 992 && (
-        <div onClick={handleClick} className="absolute top-0 right-0">
-          <button className="text-white p-3 bg-black/60">Display</button>
+        <div onClick={handleClick} className="absolute top-2 right-2">
+          <Bars3Icon className="text-white h-10 w-10" />
         </div>
       )}
     </div>
